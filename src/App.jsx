b3f052fba8c0af04bfa82620aae348731035aa65
noname.from.milkyway <noname.from.milkyway@gmail.com>
2025-09-25 19:02:32 +1000

    1:42:15

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Hero from "./components/Hero"
 import About from "./components/About"
+import Navar from "./components/Navbar"
+import Features from "./components/Features"
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navar />
       <Hero />
       <About />
+      <Features />
     </main>
   )
 }
---
+++ src/components/Features.jsx
@@ -0,0 +1,14 @@
+const Features = () => {
+    return (
+        <section className="bg-black pb-52">
+            <div className="container mx-auto px-3 md:px-10">
+                <div className="px-5 py-32">
+                    <p className="font-circular-web text-lg text-blue-50">
+                        Into the Metagame Layer
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Features
